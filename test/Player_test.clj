f64a6de02565198d0471152d017d8b3e23f70097
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2020-05-17 23:53:00 +0200

    wip

--- test/Player_test.clj
+++ test/Player_test.clj
@@ -6,3 +6,18 @@
   (testing "A space tile is free space"
     (is (is-free-space \space))
     (is (not (is-free-space \#)))))
+
+(let [grid "###################################     # #   #   # ### #   #   # #     ### # # # # # # ### # # # # # # ### #   #   #   #         #   #   #   # # ### ##### ### ### ### ##### ### # #       #   #         #   #       # ### # # # # # # # # # # # # # # ### ### #     #   # # # #   #     # ### ### ### # ### # ### # ### # ### ### #   #   #     #     #     #   #   # # # # # ### # ### ### # ### # # # # # #     #   #         #   #     # # ### ### # # # ### ### # # # ### ###     #     # #   # #   # #     #     ###################################"
+      width 35
+      height (+ (mod (count grid) width) 1)
+      row-width (+ width 1)]
+  (->> (range height)
+       (map
+         (fn [i]
+           (subs grid (* row-width i) (if (= i (- height 1))
+                                        (- (count grid) 1)
+                                        (* row-width (+ i 1))))))
+       (map (fn [row] (if (> (count row) width)
+                        (subs row 0 (- width 1))
+                        row)))
+       (clojure.string/join "\n")))
